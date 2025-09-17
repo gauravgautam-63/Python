f85678a31307e305ebdd481200d8d502dd2eaf8a
Gaurav Gautam <gautamgaurav2063@gmail.com>
2025-09-17 19:26:54 +0545

    update  work

---
+++ Day1/day1.py
@@ -0,0 +1,35 @@
+if 5 > 2:
+    print("Five is")
+else:
+    print("Two")
+
+# This is a single-line comment
+"""
+This is a 
+multi-line comment
+"""
+name = "Gaurav"
+age = 20
+print(name, age)
+
+
+x = 10        # int
+y = 3.14      # float
+z = "Python"  # str
+a = True      # bool
+
+
+a = 5
+b = 2
+print(a + b)  # Addition
+print(a ** b) # Exponentiation
+
+
+x = int(3.5)   # float to int
+y = str(10)    # int to string
+print(x, y)
+
+
+text = "Python"
+print(text[0])       # Access first character
+print(text + " 3.13") # Concatenation
