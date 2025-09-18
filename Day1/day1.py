975b3589220ae08599d03001c722ddd1cb77d99d
Gaurav Gautam <gautamgaurav2063@gmail.com>
2025-09-18 06:57:57 +0545

    update  work

--- Day1/day1.py
+++ Day1/day1.py
@@ -13,23 +13,23 @@ age = 20
 print(name, age)
 
 
-x = 10        # int
-y = 3.14      # float
+x = 10  # int
+y = 3.14  # float
 z = "Python"  # str
-a = True      # bool
+a = True  # bool
 
 
 a = 5
 b = 2
 print(a + b)  # Addition
-print(a ** b) # Exponentiation
+print(a**b)  # Exponentiation
 
 
-x = int(3.5)   # float to int
-y = str(10)    # int to string
+x = int(3.5)  # float to int
+y = str(10)  # int to string
 print(x, y)
 
 
 text = "Python"
-print(text[0])       # Access first character
-print(text + " 3.13") # Concatenation
+print(text[0])  # Access first character
+print(text + " 3.13")  # Concatenation
